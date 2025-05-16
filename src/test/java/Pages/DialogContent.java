a0370fb4da210a324559e1cc47a8fd71cd8743e6
burakboradurmaz <burakboradurmaz@gmail.com>
2025-05-16 23:55:38 +0300

    Dialog content eklendi.

--- src/test/java/Pages/DialogContent.java
+++ src/test/java/Pages/DialogContent.java
@@ -51,6 +51,145 @@ public class DialogContent extends ParentPage{
     @FindBy(xpath = "(//li[@class='authorization-link'])[1]")
     public WebElement SignOutButton;
 
+    //TC_705
+
+    @FindBy(xpath = "//a[normalize-space(text())='Sign In']")
+    public WebElement SignInButton;
+
+    @FindBy(xpath = "//input[@name='login[username]']")
+    public WebElement LoginEmail;
+
+    @FindBy(xpath = "//input[@name='login[password]']")
+    public WebElement LoginPassword;
+
+    @FindBy(xpath = "//button[@id='send2']")
+    public WebElement SignButton;
+
+    @FindBy(xpath = "//img[@alt='Hero Hoodie']")
+    public WebElement SelectProduct;
+
+    @FindBy(xpath = "//div[@option-id='169']")
+    public WebElement SelectSize;
+
+    @FindBy(xpath = "//div[@option-id='49']")
+    public WebElement SelectColor;
+
+    @FindBy(xpath = "//input[@class='input-text qty']")
+    public WebElement SelectQty;
+
+    @FindBy(xpath = "//span[text()='Add to Cart']")
+    public WebElement AddToCart;
+
+    @FindBy(xpath = "//div[@class='message-success success message']")
+    public WebElement AdditionSuccessMessage;
+
+    @FindBy(xpath = "//div[@data-block='minicart']//span[contains(@class, 'counter') and contains(@class, 'qty')]")
+    public WebElement ClickToCart;
+
+    @FindBy(xpath = "//span[text()='View and Edit Cart']")
+    public WebElement ViewAndEditCart;
+
+    @FindBy(xpath = "//a[@title='Remove item' and contains(@class, 'action-delete')]")
+    public WebElement RemoveItem;
+
+    @FindBy(xpath = "//a[text()='here']")
+    public WebElement HereToContinue;
+
+    @FindBy(xpath = "//img[@alt='Hero Hoodie']")
+    public WebElement SelectNewProduct;
+
+    @FindBy(xpath = "//div[@option-id='170']")
+    public WebElement SelectNewSize;
+
+    @FindBy(xpath = "//div[@option-id='52']")
+    public WebElement SelectNewColor;
+
+    @FindBy(xpath = "//input[@class='input-text qty']")
+    public WebElement SelectNewQty;
+
+    @FindBy(xpath = "//span[text()='Add to Cart']")
+    public WebElement AddToCartAgain;
+
+    @FindBy(xpath = "//div[@data-block='minicart']//span[contains(@class, 'counter') and contains(@class, 'qty')]")
+    public WebElement ClickToCartAgain;
+
+    @FindBy(xpath = "//span[text()='View and Edit Cart']")
+    public WebElement ViewAndEditCartAgain;
+
+    @FindBy(xpath = "//a[@title='Edit item parameters']")
+    public WebElement EditItem;
+
+    @FindBy(xpath = "//input[@class='input-text qty']")
+    public WebElement EditQty;
+
+    @FindBy(xpath = "//span[text()='Update Cart']")
+    public WebElement UpdateCart;
+
+    @FindBy(xpath = "//div[@data-bind='html: $parent.prepareMessageForHtml(message.text)']")
+    public WebElement UpdateSuccessMessage;
+
+    //TC_703
+
+    @FindBy(xpath = "//li[@class='authorization-link']//a")
+    public WebElement signHomePage;
+
+    @FindBy(xpath = "//input[@id='email']")
+    public WebElement email;
+
+    @FindBy(xpath = "//input[@id='pass']")
+    public WebElement password;
+
+    @FindBy(xpath = "//*[text()='Sign In']")
+    public WebElement signButton;
+
+    @FindBy(xpath = "//*[@class='logged-in']")
+    public WebElement accessSuccess;
+
+    @FindBy(xpath = "//*[@role='add-address']")
+    public WebElement addNewAddress;
+
+    @FindBy(id = "country")
+    public WebElement country;
+
+    @FindBy(xpath = "//*[@id='country']//option[@value='TR']")
+    public WebElement TR;
+
+    @FindBy(xpath = "//*[@title='Street Address']")
+    public WebElement streetAddress;
+
+    @FindBy(xpath = "//*[@title='City']")
+    public WebElement city;
+
+    @FindBy(id = "zip")
+    public WebElement postalCode;
+
+    @FindBy(id = "telephone")
+    public WebElement phoneNumber;
+
+    @FindBy(id = "primary_billing")
+    public WebElement buttonBillingAddress;
+
+    @FindBy(id = "primary_shipping")
+    public WebElement buttonShippingAddress;
+
+    @FindBy(xpath = "//*[@title='Save Address']")
+    public WebElement saveAddress;
+
+    @FindBy(xpath = "//*[@role='alert']/div/div")
+    public WebElement successMesage;
+
+    @FindBy(xpath = "(//*[@data-th='Actions']/a/span)[1]")
+    public WebElement edit1;
+
+    @FindBy(xpath = "(//*[@data-th='Actions']/a/span)[3]")
+    public WebElement edit2;
+
+    @FindBy(xpath = "(//*[@data-th='Actions']/a)[2]")
+    public WebElement delete;
+
+    @FindBy(xpath = "(//*[@data-role='action'])[2]")
+    public WebElement deleteOkButton;
+
 
     public WebElement getWebElement(String strElement)
     {
